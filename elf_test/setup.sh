040d1808f4fa085e6d8160e1a5b11cf11b1b58fa
vogr <contact@vogier.fr>
2022-06-03 17:02:00 +0200

    Apply patch to rust-elf to remove unrecoverables panic() calls

--- elf_test/setup.sh
+++ elf_test/setup.sh
@@ -1,10 +1,19 @@
-cd app
-git clone https://github.com/cole14/rust-elf.git
-git clone https://github.com/cole14/rust-readelf.git
-cd ..
+#!/usr/bin/env bash
 
+(
+  cd app
+  git clone https://github.com/cole14/rust-elf.git
+  git clone https://github.com/cole14/rust-readelf.git
+
+  # replace unrecoverable panics by recoverable Err()
+  patch rust-elf/src/utils.rs utils.patch
+)
+
+# Make rust-readelf build against the local version of rust-elf
 cp -f Cargo_rust-readelf.toml app/rust-readelf/Cargo.toml
 
-cd app/rust-readelf
-RUSTFLAGS="-g -C target-feature=+crt-static" cargo build
-cd ../..
+(
+  # Build rust readelf (and rust elf)
+  cd app/rust-readelf
+  RUSTFLAGS="-g -C target-feature=+crt-static" cargo build
+)
