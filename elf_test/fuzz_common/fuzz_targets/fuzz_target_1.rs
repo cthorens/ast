95b6a9b6b502f7a702a0bff81fc11c8b7219aade
vogr <contact@vogier.fr>
2022-06-03 16:52:03 +0200

    Fuzz target for rust-elf: catch parse errors

--- elf_test/fuzz_common/fuzz_targets/fuzz_target_1.rs
+++ elf_test/fuzz_common/fuzz_targets/fuzz_target_1.rs
@@ -3,29 +3,20 @@ use libfuzzer_sys::fuzz_target;
 
 use std::fs::File;
 use std::io::Write;
-use std::path::PathBuf;
 
+use elf::ParseError;
 
-fuzz_target!(|data: &[u8]| {
-    if data.len() < 500 {
-        return;
-    }
-    let fname_len = data[0] as usize;
+fuzz_target!(|data: &[u8]| {    
+    //println!("Fuzzing against file {}", fname);
 
-    println!("Fname_len = {}", fname_len);
+    if data.len() < 1 { return; }
 
-    let fname = String::from_utf8_lossy(&data[1..(1+fname_len)]);
-    
-    println!("Fuzzing against file {}", fname);
-
-    let path = PathBuf::from(fname.to_string());
-
-    let mut f = File::create(&path).expect("Cannot open input_file for writing");
+    let mut f = File::create("input.txt").expect("Cannot open input.txt for writing");
     f.write_all(&data).expect("Cannot write to input.txt");
 
-
-    let file = match elf::File::open_path(&path) {
-        Ok(f) => f,
+    let _res : Result<(),()> = match elf::File::open_path("input.txt") {
+        Ok(_f) => Ok(()),
+        Err(ParseError) => Ok(()),
         Err(e) => panic!("Error: {:?}", e),
     };
 });
