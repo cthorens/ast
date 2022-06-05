35e8dee1773378a08bf90cb303cc35640a10a208
vogr <contact@vogier.fr>
2022-06-05 13:43:33 +0200

    Use most recent scripts in svgcleaner (to make it compatible with make_fuzz_targets)

--- svgcleaner_test/execute.sh
+++ svgcleaner_test/execute.sh
@@ -1,10 +1,27 @@
-cd app/svgcleaner
-RUSTFLAGS="-g -C target-feature=+crt-static" cargo build
-cd ../..
+#!/usr/bin/env bash
 
+SCRIPTS_DIR="$(realpath "../lava-scripts")"
+
+echo "Compile the executable..."
+
+(
+    cd app/svgcleaner
+    RUSTFLAGS="-g -C target-feature=+crt-static" cargo build
+)
+
+# panda can't handle long paths in folder to copy
+rm -rf app/svgcleaner/target/debug/build
+
+echo "Run panda dynamic analysis..."
 python3 panda.py
-python3 parse_logs.py svgcleaner
 
+
+echo "Parse logs to get tainted asm lines..."
+python3 "$SCRIPTS_DIR/parse_logs.py" svgcleaner
+
+echo "Convert tainted asm lines to src lines..."
 cat addr.txt | addr2line  -e app/svgcleaner/target/debug/svgcleaner | sort --unique | grep app > lines.txt
 
-python3 inject.py
+
+echo "Inject bugs..."
+python3 "$SCRIPTS_DIR/inject.py"
--- svgcleaner_test/panda.py
+++ svgcleaner_test/panda.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from pandare import Panda
 
 panda = Panda(generic="x86_64")
